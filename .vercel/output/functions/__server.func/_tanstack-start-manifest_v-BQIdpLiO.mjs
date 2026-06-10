d1a8d6111baf0076ca9df1ca87abff86a8a69ad2
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-06-10 17:14:49 +0200

    remove unesseary files

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-BQIdpLiO.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/BOOK/Documents/Projects/newPortfolio/src/routes/__root.tsx", children: ["/", "/api/public/contact"], preloads: ["/assets/index-mlvi9G3u.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-mlvi9G3u.js" } }] }, "/": { filePath: "C:/Users/BOOK/Documents/Projects/newPortfolio/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CWIjSOgb.js"] } } });
-export {
-  tsrStartManifest
-};
